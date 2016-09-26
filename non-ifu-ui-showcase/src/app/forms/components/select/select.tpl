61fb0b199c0ed70ea3e6028d8c018371b8a2f3da
Gurudath Reddy <gurudath.jcreddy@gmail.com>
2016-09-26 09:47:33 +0530

    feat(showcase): formly components

--- non-ifu-ui-showcase/src/app/forms/components/select/select.tpl
+++ non-ifu-ui-showcase/src/app/forms/components/select/select.tpl
@@ -1,11 +1,14 @@
 <div layout="row" layout-xs="column" layout-padding flex>
-    <div flex>
-        <h2>UI Showcase Form Components</h2>
-
+    <div flex-gt-sm="70" flex>
+        <h2>UI Showcase Form Components - Select</h2>
         <div>
             <form name="vm.componentsForm" ng-submit="vm.submit(vm.componentsForm.$valid)" non-accessible-form novalidate>
                 <formly-form model="vm.components.data" fields="vm.components.fields" form="vm.componentsForm"></formly-form>
             </form>
         </div>
+    </div>
+    <div flex-gt-sm="30" flex>
+        <h2>Form Model</h2>
+        <div>{{vm.components.data | json}}</div>
     </div>
 </div>
--- non-ifu-ui-showcase/src/app/forms/forms.routes.ts
+++ non-ifu-ui-showcase/src/app/forms/forms.routes.ts
@@ -13,12 +13,72 @@ export class UiRouterConfig {
                     }
                 }
             })
-            .state('app.ui-showcase.forms-components', {
-                url: '/ui-showcase/forms/components',
+            .state('app.ui-showcase.forms-components-input', {
+                url: '/ui-showcase/forms/components/input',
                 views   : {
                     'content@app': {
-                        template: require('./components/components.tpl'),
-                        controller: 'ComponentsController',
+                        template: require('./components/input/input.tpl'),
+                        controller: 'InputComponentsController',
+                        controllerAs: 'vm'
+                    }
+                }
+            })
+            .state('app.ui-showcase.forms-components-checkbox', {
+                url: '/ui-showcase/forms/components/checkbox',
+                views   : {
+                    'content@app': {
+                        template: require('./components/checkbox/checkbox.tpl'),
+                        controller: 'CheckboxComponentsController',
+                        controllerAs: 'vm'
+                    }
+                }
+            })
+            .state('app.ui-showcase.forms-components-radiobutton', {
+                url: '/ui-showcase/forms/components/radiobutton',
+                views   : {
+                    'content@app': {
+                        template: require('./components/radiobutton/radiobutton.tpl'),
+                        controller: 'RadiobuttonComponentsController',
+                        controllerAs: 'vm'
+                    }
+                }
+            })
+            .state('app.ui-showcase.forms-components-select', {
+                url: '/ui-showcase/forms/components/select',
+                views   : {
+                    'content@app': {
+                        template: require('./components/select/select.tpl'),
+                        controller: 'SelectComponentsController',
+                        controllerAs: 'vm'
+                    }
+                }
+            })
+            .state('app.ui-showcase.forms-components-datepicker', {
+                url: '/ui-showcase/forms/components/datepicker',
+                views   : {
+                    'content@app': {
+                        template: require('./components/datepicker/datepicker.tpl'),
+                        controller: 'DatepickerComponentsController',
+                        controllerAs: 'vm'
+                    }
+                }
+            })
+            .state('app.ui-showcase.forms-components-switch', {
+                url: '/ui-showcase/forms/components/switch',
+                views   : {
+                    'content@app': {
+                        template: require('./components/switch/switch.tpl'),
+                        controller: 'SwitchComponentsController',
+                        controllerAs: 'vm'
+                    }
+                }
+            })
+            .state('app.ui-showcase.forms-components-textarea', {
+                url: '/ui-showcase/forms/components/textarea',
+                views   : {
+                    'content@app': {
+                        template: require('./components/textarea/textarea.tpl'),
+                        controller: 'TextareaComponentsController',
                         controllerAs: 'vm'
                     }
                 }
@@ -29,6 +89,16 @@ export class UiRouterConfig {
                     'content@app': {
                         template: require('./dynamic-validations/dynamic-validations.tpl'),
                         controller: 'DynamicValidationsController',
+                        controllerAs: 'vm'
+                    }
+                }
+            })
+            .state('app.ui-showcase.forms-layouts', {
+                url: '/ui-showcase/forms/layouts',
+                views   : {
+                    'content@app': {
+                        template: require('./layouts/layouts.tpl'),
+                        controller: 'LayoutsController',
                         controllerAs: 'vm'
                     }
                 }
