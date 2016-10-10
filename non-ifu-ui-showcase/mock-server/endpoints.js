8d9399f17bdddb61235310204f327bb7284a4e8c
Superman <superman08998@gmail.com>
2016-10-10 16:38:08 +0530

    feat(showcase): implement formly components

--- non-ifu-ui-showcase/mock-server/endpoints.js
+++ non-ifu-ui-showcase/mock-server/endpoints.js
@@ -8,7 +8,10 @@ function getLocalization(req, res, next) {
             FIELD_MIN_LENGTH_NEEDED: 'Minimum Length Not Reached!',
             FIELD_MIN_VALUE_NEEDED: 'Minimum Value Not reached!',
             FIELD_MAX_LENGTH_EXCEEDED_VARIABLE: '{{value}} should be less than Maxlength {{maxlength}}!',
-            FIELD_WITH_TRANSLATION: 'Field With Translation'
+            FIELD_WITH_TRANSLATION: 'Field With Translation',
+            FIELD_NOT_DATE: "The entered value is not a date!",
+            FIELD_MIN_DATE_NEEDED: "Date is too early!",
+            FIELD_MAX_DATE_EXCEEDED: "Date is too late!"
         });
 }
 
