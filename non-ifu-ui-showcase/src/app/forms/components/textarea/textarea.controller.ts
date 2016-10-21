36ab6498a526583deb1f880003c995d5970adb72
Superman <superman08998@gmail.com>
2016-10-21 18:00:59 +0530

    feat(form-submission): implement form submission framework

--- non-ifu-ui-showcase/src/app/forms/components/textarea/textarea.controller.ts
+++ non-ifu-ui-showcase/src/app/forms/components/textarea/textarea.controller.ts
@@ -1,10 +1,12 @@
+import { FormConfiguration } from '@norn/non-framework';
+
 const componentFields: any = require('./textarea.form.json');
 
 export class TextareaComponentsController {
 
-    public components: any;
+    public formConfiguration: FormConfiguration;
 
     constructor() {
-        this.components = { data: {}, fields: componentFields };
+        this.formConfiguration = { model: {}, fields: componentFields };
     }
 }
