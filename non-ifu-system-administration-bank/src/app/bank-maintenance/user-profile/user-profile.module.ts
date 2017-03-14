37315080da852257d7c448da157b6faab3c09c94
BatMan <batman82704@gmail.com>
2017-03-14 13:23:00 +0530

    create generic grid methods for common actions

--- non-ifu-system-administration-bank/src/app/bank-maintenance/user-profile/user-profile.module.ts
+++ non-ifu-system-administration-bank/src/app/bank-maintenance/user-profile/user-profile.module.ts
@@ -7,14 +7,12 @@ import {UiRouterConfig} from './user-profile.routes';
 
 // Controller
 import {BABankUserProfileFormController} from './user-profile-form.controller';
-import {BABankUserProfileListController} from './user-profile-list.controller';
 
 // Service
 import {BABankUserProfileService} from './user-profile.service';
 
 angular.module(moduleName, [])
     .controller('BABankUserProfileFormController', BABankUserProfileFormController)
-    .controller('BABankUserProfileListController', BABankUserProfileListController)
     .service('BABankUserProfileService', BABankUserProfileService)
     .config(UiRouterConfig);
 
